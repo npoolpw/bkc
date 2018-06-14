20bae76ccdacbd6ab7ba761fde91d6b1ab337a2e
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-14 22:03:37 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+ar api = "https://api.npool.pw/bkc";
+
+var poolHost = "ca.npool.pw";
+
+var blockExplorer = "http://explorer.balkancoin.org/";
+
+var cryptonatorWidget = ["BKC-BTC", "BTC-USD"];
+
+var blockchainExplorer = "http://explorer.balkancoin.org/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.balkancoin.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["npool.pw/bkc", "https://api.npool.pw/bkc"],
+        ["miner.balkancoin.org", "http://miner.balkancoin.org:8147"],
+        ["bkc.mininggood.com", "http://us2.mininggood.com:8147"],
+        ["bkc.euminingpool.com", "http://bkc.euminingpool.com:8147"]
+];
